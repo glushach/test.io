82f9934845cc8c003e7a1c99da7987e29d246174
Aleksandr <shoppingplaneta@gmail.com>
2020-07-08 20:46:25 +0300

    Сверстал секцию Хедер и промо

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function(){
+  $('#phone').focus(function() {
+    $('#hint').hide();
+  });
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99"); 
+  
+  $('#phone').blur(function() {
+    if ($(this).val().trim() === '') {
+        $('#hint').show();
+    }
+  });
+});
